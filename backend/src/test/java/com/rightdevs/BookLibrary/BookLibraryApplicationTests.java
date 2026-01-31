5bdfa19d320beb6327c8cb6af0250e2b47616992
krupal.vasani <krupalvasani222@gmail.com>
2026-01-31 16:16:05 +0530

    Initial commit

---
+++ backend/src/test/java/com/rightdevs/BookLibrary/BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rightdevs.BookLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
